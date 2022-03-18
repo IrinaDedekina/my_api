38ee39e244d7a49d6e739d3cef96bdfa516cdfea
IrinaDedekina <dedekina92@yandex.ru>
2022-03-18 17:09:01 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :users
+  get 'users/index'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/update'
+  get 'users/delete'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
