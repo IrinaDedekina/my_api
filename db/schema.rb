0c60f9948b34ec32471b0a68c6b9284348ad73e1
IrinaDedekina <dedekina92@yandex.ru>
2022-03-18 17:34:31 +0300

    add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_134100) do
+ActiveRecord::Schema.define(version: 2022_03_18_143227) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
